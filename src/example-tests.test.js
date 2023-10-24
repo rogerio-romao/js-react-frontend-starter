1ae23d65f47c3722ab22151ea1a656c3f391f6aa
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2023-10-24 13:37:44 +0100

    fix(test): remove msw api request test

--- src/example-tests.test.js
+++ src/example-tests.test.js
@@ -1,16 +1,4 @@
-import { graphql, http } from 'msw';
-import { setupServer } from 'msw/node';
-import {
-    afterAll,
-    afterEach,
-    beforeAll,
-    beforeEach,
-    describe,
-    expect,
-    it,
-    test,
-    vi,
-} from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, test, vi } from 'vitest';
 import {
     executeAfterTwoHours,
     executeEveryMinute,
@@ -110,56 +98,6 @@ describe('reading messages', () => {
         expect(mock).toHaveBeenCalledTimes(3);
     });
 });
-
-// Mocking API requests
-
-const posts = [
-    {
-        userId: 1,
-        id: 1,
-        title: 'first post title',
-        body: 'first post body',
-    },
-    {
-        userId: 2,
-        id: 5,
-        title: 'second post title',
-        body: 'second post body',
-    },
-    {
-        userId: 3,
-        id: 6,
-        title: 'third post title',
-        body: 'third post body',
-    },
-];
-
-const jsonPlaceHolder = graphql.link('https://jsonplaceholder.ir/graphql');
-// Define handlers that catch the corresponding requests and returns the mock data.
-export const handlers = [
-    http.get('https://jsonplaceholder.typicode.com/posts', (_req, res, ctx) =>
-        res(ctx.status(200), ctx.json(posts))
-    ),
-
-    jsonPlaceHolder.query('posts', (_req, res, ctx) =>
-        res(
-            ctx.data({
-                posts,
-            })
-        )
-    ),
-];
-
-const server = setupServer(...handlers);
-
-// Start server before all tests
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
-
-//  Close server after all tests
-afterAll(() => server.close());
-
-// Reset handlers after each test `important for test isolation`
-afterEach(() => server.resetHandlers());
 
 // Timers
 // eslint-disable-next-line no-console
