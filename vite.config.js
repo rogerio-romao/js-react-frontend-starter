ccae9309f4c61cb678dcc7ebad7d06d9dbcb0194
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2023-10-24 21:51:56 +0100

    feat(tests): Playwright added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [react()],
+    test: {
+        include: ['**/*.test.?(c|m)[jt]s?(x)'],
+    },
 });
